bf0b7d9f1668a71a7494e31c18012a209a352d32
Muhammad Hasan <hasanamir8901@gmail.com>
2025-08-07 22:04:06 +0500

    Implement complete and edit button

--- gui.py
+++ gui.py
@@ -7,9 +7,12 @@ add_button = sg.Button('Add')
 list_box = sg.Listbox(values=f.get_todos(), key='todos',
                       enable_events=True, size=(45, 10))
 edit_button = sg.Button('Edit')
+complete_button = sg.Button('Complete')
+exit_button = sg.Button('Exit')
 
 window = sg.Window('My To-Do App',
-                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                   layout=[[label], [input_box, add_button],
+                   [list_box, edit_button, complete_button], [exit_button]],
                    font=('Helvetica', 20))
 while True:
     event, value = window.read()
@@ -34,6 +37,15 @@ while True:
             window['todos'].update(values=todos)
         case 'todos':
             window['todo'].update(value=value['todos'][0])
+        case 'Complete':
+            todo_to_complete = value['todos'][0]
+            todos = f.get_todos()
+            todos.remove(todo_to_complete)
+            f.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
+        case 'Exit':
+            break
         case sg.WIN_CLOSED:
             break
 
