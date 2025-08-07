08926fadc03afbcf5bed20c418e44fd50b658dd7
Muhammad Hasan <hasanamir8901@gmail.com>
2025-08-07 23:46:15 +0500

    prepare file for creating standalone execution

--- gui.py
+++ gui.py
@@ -1,8 +1,16 @@
-import functions as f
+import os
 import time
+import functions as f
 import FreeSimpleGUI as sg
 
+
 sg.theme("Black")
+
+
+if not os.path.exists('todos.txt'):
+    with open('todos.txt', 'w') as file:
+        pass
+
 
 clock = sg.Text('', key='clock')
 label = sg.Text('Type in a To-Do')
@@ -14,10 +22,13 @@ edit_button = sg.Button('Edit')
 complete_button = sg.Button('Complete')
 exit_button = sg.Button('Exit')
 
+
 window = sg.Window('My To-Do App',
                    layout=[[clock], [label], [input_box, add_button],
                    [list_box, edit_button, complete_button], [exit_button]],
                    font=('Helvetica', 20))
+
+
 while True:
     event, value = window.read(timeout=200)
     window['clock'].update(value=time.strftime("%b %d, %Y %H:%M"))
@@ -31,6 +42,7 @@ while True:
             todo.append(new_todo)
             f.write_todos(todo)
             window['todos'].update(values=todo)
+
         case 'Edit':
             try:
                 todo_to_edit = value['todos'][0]
@@ -43,8 +55,10 @@ while True:
                 window['todos'].update(values=todos)
             except IndexError:
                 sg.popup("Please select an item first!", font=('helvetica', 20))
+
         case 'todos':
             window['todo'].update(value=value['todos'][0])
+
         case 'Complete':
             try:
                 todo_to_complete = value['todos'][0]
@@ -55,8 +69,10 @@ while True:
                 window['todo'].update(value='')
             except IndexError:
                 sg.popup("Please select an item first!", font=('helvetica', 20))
+
         case 'Exit':
             break
+
         case sg.WIN_CLOSED:
             break
 
