a639e7da1e24f8e2e99c9bf60d0214f4cbec05da
Muhammad Hasan <hasanamir8901@gmail.com>
2025-08-08 02:19:57 +0500

    complete fully responsive web app

--- web.py
+++ web.py
@@ -1,11 +1,23 @@
 import streamlit as st
 import functions as f
 
-st.title("My To-Do App")
-
 todos = f.get_todos()
 
-for todo in todos:
-    st.checkbox(todo)
+def add_todo():
+    todo = st.session_state['new_todo'] + "\n"
+    todos.append(todo)
+    f.write_todos(todos)
+    st.session_state['new_todo'] = ""
 
-st.text_input(label="", placeholder="Enter a new Todo")
+st.title("My To-Do App")
+
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        f.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
+
+st.text_input(label="", placeholder="Enter a new Todo",
+              on_change=add_todo, key='new_todo')
